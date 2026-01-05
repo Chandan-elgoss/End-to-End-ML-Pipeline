0d603cf792eef121b1ad57fe3cecd52e5a569db4
Chandan-elgoss <Chandan.kumar@elgoss.com>
2026-01-05 15:54:07 +0530

    Setup/Requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# End-to-End-ML-Pipeline
+# End-to-End-ML-
+
+```
+conda create -n mlproject python=3.8 -y
+
+```
+
+```
+conda activate mlproject
+
+```
+
+```
+pip install requirements.txt
+
+```
---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Package metadata
+PACKAGE_NAME = "End-to-End-ML-Pipeline"
+VERSION = "0.1.0"
+AUTHOR = "chandan-elgoss"
+AUTHOR_EMAIL = "chandan.kumar@elgoss.com"
+DESCRIPTION = "A comprehensive end-to-end machine learning pipeline"
+URL = f"https://github.com/{AUTHOR}/{PACKAGE_NAME}"
+SRC_REPO = "mlProject"
+
+
+PYTHON_REQUIRES = ">=3.8"
+
+# Dependencies
+INSTALL_REQUIRES = [
+    "pandas>=1.3.0",
+    "numpy>=1.21.0",
+    "scikit-learn>=1.0.0",
+    "matplotlib>=3.4.0",
+    "seaborn>=0.11.0",
+]
+
+EXTRAS_REQUIRE = {
+    "dev": ["pytest>=6.0", "black>=21.0"],
+}
+
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name=SRC_REPO,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    projects_urls={
+        "Bug Tracker": URL/"issues",
+        },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=CLASSIFIERS,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+)
